812438c21e83fa379c5aaaa8926c09128b0e3bfb
lenovo <yosepalexsander9@gmail.com>
2021-07-16 12:18:56 +0700

    done

--- script.js
+++ script.js
@@ -1,28 +1,22 @@
 let todos = [];
 
-const showGrade = document.getElementById("show-grade");
+const showType = document.getElementById("show-type");
 const btnSubmit = document.getElementById("btn-submit");
 
 btnSubmit.addEventListener("click", () => {
   const input = document.getElementById("input-value");
 
-  const result = checkGrade(input.value);
+  const result = checkOddEven(input.value);
 
   const element = `<p class="text-center">${result}<p>`;
 
-  showGrade.innerHTML = element;
+  showType.innerHTML = element;
 });
 
-function checkGrade(value) {
-  if (value >= 90) {
-    return "Your grade is A+, Marvelous!!";
-  } else if (value > 80 && value <= 90) {
-    return "Your grade is A, Excellent!";
-  } else if (value > 70 && value <= 80) {
-    return "Your grade is B, Good";
-  } else if (value >= 60 && value <= 70) {
-    return "Your grade is C, Not Bad";
+function checkOddEven(value) {
+  if (value % 2 !== 0) {
+    return "Odd Number"
   } else {
-    return "You must take remedial!!!";
+    return "Even Number"
   }
 }
