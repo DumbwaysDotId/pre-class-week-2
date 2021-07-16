aaa6b58e5cf53b1414b39ffba388696510071425
lenovo <yosepalexsander9@gmail.com>
2021-07-16 11:35:53 +0700

    done

--- script.js
+++ script.js
@@ -1,33 +1,30 @@
 let todos = [];
 
-const showGrade = document.getElementById("show-grade");
+const showGreeting = document.getElementById("show-greeting");
 const btnSubmit = document.getElementById("btn-submit");
 
 btnSubmit.addEventListener("click", () => {
-  const input = document.getElementById("input-value");
+  
+  const inputName = document.getElementById("input-name");
+  const dateTime = document.getElementById("input-time");
 
-  if (!input.value) {
-    alert("value input must not be empty");
-    return;
-  }
-
-  const result = checkGrade(input.value);
-
+  const date = new Date(dateTime.value);
+  const result = greeting(date.getHours(), inputName.value);
+  
   const element = `<p class="text-center">${result}<p>`;
-
-  showGrade.innerHTML = element;
+  showGreeting.innerHTML = element;
 });
 
-function checkGrade(value) {
-  if (value >= 90) {
-    return "Your grade is A+, Marvelous!!";
-  } else if (value > 80 && value <= 90) {
-    return "Your grade is A, Excellent!";
-  } else if (value > 70 && value <= 80) {
-    return "Your grade is B, Good";
-  } else if (value >= 60 && value <= 70) {
-    return "Your grade is C, Not Bad";
+function greeting(hour, userName) {
+  if (hour >= 5 && hour < 12) {
+    return `Good Morning, ${userName}`;
+  } else if (hour >= 12) {
+    if (hour >= 12 && hour < 16) {
+      return `Good Afternoon, ${userName}`;
+    } else {
+      return `Good Evening, ${userName}`;
+    }
   } else {
-    return "You must take remedial!!!";
+    return `Good Night, ${userName}`;
   }
 }
