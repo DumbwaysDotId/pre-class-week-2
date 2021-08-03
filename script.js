7d10e411385b218236f2c34265d72f2a5c6bde32
yosep <yosepalexsander9@gmail.com>
2021-08-03 14:31:49 +0700

    update

--- script.js
+++ script.js
@@ -1,40 +1,36 @@
 const showResult = document.getElementById("show-result");
-const additionButton = document.getElementById("btn-addition");
-const subtractionButton = document.getElementById("btn-subtraction");
-const multiplicationButton = document.getElementById("btn-multiplication");
-const divisionButton = document.getElementById("btn-division");
 
 const operand1 = document.getElementById("input-operand-1");
 const operand2 = document.getElementById("input-operand-2");
 
-additionButton.addEventListener("click", () => {
+function addition() {
   const result = parseInt(operand1.value) + parseInt(operand2.value);
 
   const element = `<p class="text-center">${result}</p>`;
 
   showResult.innerHTML = element;
-});
+}
 
-subtractionButton.addEventListener("click", () => {
+function subtraction() {
   const result = parseInt(operand1.value) - parseInt(operand2.value);
 
   const element = `<p class="text-center">${result}</p>`;
 
   showResult.innerHTML = element;
-});
+}
 
-multiplicationButton.addEventListener("click", () => {
+function multiplication() {
   const result = parseInt(operand1.value) * parseInt(operand2.value);
 
   const element = `<p class="text-center">${result}</p>`;
 
   showResult.innerHTML = element;
-});
+}
 
-divisionButton.addEventListener("click", () => {
+function division() {
   const result = parseInt(operand1.value) / parseInt(operand2.value);
 
   const element = `<p class="text-center">${result}</p>`;
 
   showResult.innerHTML = element;
-});
+}
