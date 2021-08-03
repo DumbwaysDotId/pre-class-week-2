cd77a0a2b990e231b92002dbf466edd1b71a7d57
yosep <yosepalexsander9@gmail.com>
2021-08-03 15:12:43 +0700

    update

--- script.js
+++ script.js
@@ -1,17 +1,13 @@
-let todos = [];
-
 const showType = document.getElementById("show-type");
 const form = document.getElementById("form");
 
-form.addEventListener("submit", (e) => {
-  e.preventDefault();
-
+function performCheckNumber() {
   const input = document.getElementById("input-value");
   const result = checkOddEven(input.value);
   const element = `<p class="text-center">${result}<p>`;
   
   showType.innerHTML = element;
-});
+}
 
 function checkOddEven(value) {
   if (value % 2 !== 0) {
